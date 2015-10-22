88f69c134bf81f48b6a5df2ce1535fcd496266e3
Jeffrey Young <jeffreeyoung@yahoo.com>
2015-10-22 16:17:00 -0600

    adding comments

--- kmer_frequency.py
+++ kmer_frequency.py
@@ -1,4 +1,9 @@
+#paul is doing this
 def remove_mis_reads(kmers):
+    #ACG
+    #ACG
+    #TTA
+
     #get frequency of each kmer
     #remove kmers with low frequency
     #example... if average kmer frequency is 20, remove kmers with frequency less than 4
