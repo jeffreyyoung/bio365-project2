733a0ae54fc7e74ca11895c237d430a71354215f
Jeffrey Young <jeffreeyoung@yahoo.com>
2015-10-23 11:08:02 -0600

    adding results

---
+++ run_best.sh
@@ -0,0 +1,9 @@
+python de_bruijn_assembler.py data-sets/synthetic.example.noerror.small.fasta 16 > best-results/synthetic.example.noerror.small.fasta.16.output
+python de_bruijn_assembler.py data-sets/synthetic.noerror.small.fasta 21 > best-results/synthetic.noerror.small.fasta.21.output
+python de_bruijn_assembler.py data-sets/synthetic.noerror.large.fasta 28 > best-results/synthetic.noerror.large.fasta.28.output
+python de_bruijn_assembler.py data-sets/real.error.small.fasta 62 > best-results/real.error.small.fasta.62.output
+
+#synthetic.example.noerror.small.fasta - 16
+#synthetic.noerror.small.fasta - 21
+#synthetic.noerror.large.fasta - 28
+#real.error.small.fasta - 62
--- run_on_all_fastas.sh
+++ run_on_all_fastas.sh
@@ -1,4 +1,14 @@
-for fasta in real.error.large.fasta real.error.small.fasta synthetic.example.noerror.small.fasta synthetic.noerror.large.fasta synthetic.noerror.small.fasta
+#real.error.large.fasta real.error.small.fasta synthetic.example.noerror.small.fasta synthetic.noerror.large.fasta synthetic.noerror.small.fasta
+for fasta in real.error.large.fasta
 do
- python de_bruijn_assembler.py data-sets/$fasta 13 > data-sets-results/$fasta.output
+  echo $fasta
+  a=5
+
+  while [ $a -lt 100 ]
+  do
+     echo $a
+     python de_bruijn_assembler.py data-sets/$fasta $a > data-sets-results/$fasta.$a.output
+     ((a+=1))
+  done
+  #python de_bruijn_assembler.py data-sets/$fasta 13 > data-sets-results/$fasta.output
 done
